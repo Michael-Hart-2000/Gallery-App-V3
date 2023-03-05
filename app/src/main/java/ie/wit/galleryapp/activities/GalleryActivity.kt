5aff0ddc0591093cbdf308b0b6049f39c5167653
Your Name <you@example.com>
2023-03-05 22:54:16 +0000

    -Create ArrayList of Gallerys

--- app/src/main/java/ie/wit/galleryapp/activities/GalleryActivity.kt
+++ app/src/main/java/ie/wit/galleryapp/activities/GalleryActivity.kt
@@ -1,17 +1,19 @@
 package ie.wit.galleryapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import ie.wit.galleryapp.databinding.ActivityGalleryBinding
 import ie.wit.galleryapp.models.GalleryModel
 import timber.log.Timber
 import timber.log.Timber.i
 
+
 class GalleryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGalleryBinding
     var gallery = GalleryModel()
+    val gallerys = ArrayList<GalleryModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +29,10 @@ class GalleryActivity : AppCompatActivity() {
         binding.btnAdd.setOnClickListener() {
             gallery.title = binding.galleryTitle.text.toString()
             if (gallery.title.isNotEmpty()) {
+                gallerys.add(gallery.copy())
                 i("add Button Pressed: $gallery.title")
+                for (i in gallerys.indices)
+                { i("Gallery[$i]:${this.gallerys[i]}") }
             }
             else {
                 Snackbar
