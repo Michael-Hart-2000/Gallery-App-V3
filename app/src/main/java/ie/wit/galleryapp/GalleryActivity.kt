4987db6c1548f1b24a481aed5eb5152e4ea07d50
Your Name <you@example.com>
2023-03-05 22:22:01 +0000

    -Add Event Handling

--- app/src/main/java/ie/wit/galleryapp/GalleryActivity.kt
+++ app/src/main/java/ie/wit/galleryapp/GalleryActivity.kt
@@ -2,16 +2,36 @@ package ie.wit.galleryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.wit.galleryapp.databinding.ActivityGalleryBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class GalleryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGalleryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gallery)
+
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
 
-        i("Gallery Activity started..")
+        i("Gallery Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val galleryTitle = binding.galleryTitle.text.toString()
+            if (galleryTitle.isNotEmpty()) {
+                i("add Button Pressed: $galleryTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+
     }
 }
