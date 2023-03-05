2c7381045543745a6139715f0b6d93a809dbb0a9
Your Name <you@example.com>
2023-03-05 21:34:26 +0000

    -Logging Event Handling in Kotlin

--- app/src/main/java/ie/wit/galleryapp/GalleryActivity.kt
+++ app/src/main/java/ie/wit/galleryapp/GalleryActivity.kt
@@ -2,10 +2,16 @@ package ie.wit.galleryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Gallery Activity started..")
     }
 }
